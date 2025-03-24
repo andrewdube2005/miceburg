dcf67ecadc1e572607e0b6f9ba2be2e6ab06cc4b
andrewdube2005 <mthemboandrew2005@gmail.com>
2025-03-24 21:12:00 +0200

    done

--- app/jobs-for-you/page.tsx
+++ app/jobs-for-you/page.tsx
@@ -6,43 +6,64 @@ export default function page() {
     {
       id: 1,
       title: "Consultant",
-      imgUrl: "/images/bg.jpg",
-      salary: "300",
-      description: "Looking for qualified personell.",
+      location: "Harare",
+      experience: "5",
+      deadline: "30 June",
+      status: "Open",
     },
     {
       id: 2,
       title: "Manager",
-      imgUrl: "/images/bg.jpg",
-      salary: "450",
-      description: "Looking for qualified personell.",
+      location: "Gweru",
+      experience: "3",
+      deadline: "30 June",
+      status: "Open",
+    },
+    {
+      id: 3,
+      title: "Programmer",
+      location: "Bulawayo",
+      experience: "2",
+      deadline: "10 June",
+      status: "Open",
+    },
+    {
+      id: 4,
+      title: "Plumber",
+      location: "Harare",
+      experience: "1",
+      deadline: "2 March",
+      status: "Closed",
     },
   ];
+
 
   return (
     <div className="flex flex-col gap-10 items-start px-20 py-[70px]">
       <p className="text-[25px] text-[#656565] pt-10">Jobs For You</p>
       <div
-        className="
-            grid 
-            grid-cols-1 
-            md:grid-cols-2
-            lg:grid-cols-3
-            xl:grid-cols-4
-            gap-x-5
-            gap-y-5
-          "
-      >
-        {jobs.map((job, index) => (
-          <Jobs
-            key={index}
-            title={job.title}
-            imgUrl={job.imgUrl}
-            salary={job.salary}
-            description={job.description}
-          />
-        ))}
-      </div>
+                className="
+                  grid 
+                  grid-cols-1 
+                  md:grid-cols-2
+                  lg:grid-cols-4
+                  xl:grid-cols-4
+                  gap-x-5
+                  gap-y-5
+                  w-full
+                "
+              >
+                {jobs.map((job, index) => (
+                  <Jobs
+                    key={index}
+                    title={job.title}
+                    location={job.location}
+                    experience={job.experience}
+                    deadline={job.deadline}
+                    status={job.status}
+                  />
+                ))}
+              </div>
     </div>
   );
 }
